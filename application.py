ee334fec6b95ecc52f9f9b0414a065a87da24ba2
Виктор Антропов <antropovvic1997@yandex.ru>
2024-02-14 23:49:13 +0300

    TOKEN

--- application.py
+++ application.py
@@ -3,7 +3,6 @@ from telegram.ext import Application
 from core.settings import settings
 from handlers import register_all_handlers
 
-# TOKEN = '6743069114:AAHupo1MXiGypPXgXMftSrgI5JEtkRyk6DU'
 
 def main() -> None:  # noqa
     '''Запуск бота.'''
